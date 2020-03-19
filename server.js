55ebe0ca4751674f058eded64980c2f1864cecaf
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2020-03-18 20:10:49 -0400

    added line hope routing works

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3010;
 const app = express();
 const routes = require("./routes")
+const path = require('path');
 // const mongoose = require("mongoose");
 
 
@@ -10,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 }
 
 app.use(routes);
